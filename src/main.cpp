6c0d6ad931fb56eeedbf7103a81417852974347a
poleselalex <poleselalex@hotmail.com>
2023-01-23 08:41:22 +0100

    updated chip8.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
-﻿#include <iostream>
-#include <chrono>
-#include <thread>
+﻿#include "chip8.hpp"
 #include "window.hpp"
-#include "chip8.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 int wmain(int argc, char argv[]) {
-	chip8 emulator = chip8();
-	window screen = window(&emulator, 1024, 512);
+  chip8 emulator = chip8();
+  window screen = window(&emulator, 1024, 512);
 
-	screen.init();
-	// Attempt to load ROM
-	emulator.load("ROM_PATH"); // You can use whatever rom you want from roms directory
+  screen.init();
+  // Attempt to load ROM
+  emulator.load(
+      "ROM_PATH"); // You can use whatever rom you want from roms directory
 
-	// Emulation loop
-	while (true) {
-		emulator.cycle();
-		screen.processEvent();
+  // Emulation loop
+  while (true) {
+    emulator.cycle();
+    screen.processEvent();
 
-		if (emulator.draw) {
-			!emulator.draw;
-			screen.refresh();
-		}
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(2));
-	}
+    if (emulator.draw) {
+      !emulator.draw;
+      screen.refresh();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+  }
 }
